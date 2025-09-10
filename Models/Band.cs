b2eb318f2adf5382d39e8ac9d31a5e12a54bbb4f
martijn1319 <martijn1319@gmail.com>
2025-09-10 14:04:39 +0200

    Basic API working

--- Models/Band.cs
+++ Models/Band.cs
@@ -1,14 +1,13 @@
 using System;
 
 
-namespace BandDB.Models 
+namespace BandDB.Models;
+
+public class Band 
 {
-    public class Band 
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Genre { get; set; } = "";
-        public int YearFormed { get; set; }
-        public bool IsActive { get; set; }
-    }
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Genre { get; set; } = "";
+    public int YearFormed { get; set; }
+    public bool IsActive { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using BandDB.Data;
+using BandDB.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=banddb.db"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
